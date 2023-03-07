3d4188e7d7b33bcaab16adaa55b2fa6b904054f9
Mankar123sayu <sayumankar77@gmail.com>
2023-03-08 00:07:46 +0530

    [sayunkta] Add:linecomparisionfilee

---
+++ LineComparision/CalculateLength.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class CalculateLength
+    {
+        int x1, x2, y1, y2;
+        public CalculateLength(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public double Calculate()
+        {
+            double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(result);
+            return result;
+        }
+        public void CheckEquality(double line1, double line2)
+        {
+            if (line1.Equals(line2))
+                Console.WriteLine("Both are line equal");
+            else
+                Console.WriteLine("Lines are not equal");
+        }
+    }
+}
--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace LineComparision
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linecomparision problem");
+            Console.WriteLine("Enter x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            CalculateLength calculateLength1 = new CalculateLength(x1, x2, y1, y2);
+            double line1 = calculateLength1.Calculate();
+            CalculateLength calculateLength2 = new CalculateLength(2, 3, 4, 5);
+            double line2 = calculateLength2.Calculate();
+            calculateLength2.CheckEquality(line1, line2);
+        }
+    }
+}
