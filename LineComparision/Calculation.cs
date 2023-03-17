0869a8f319f59e1615779ceb829342adcd926d49
Mankar123sayu <sayumankar77@gmail.com>
2023-03-17 10:52:49 +0530

    Linefiles

---
+++ LineComparision/Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class Calculation
+    {
+        int x1, x2, y1, y2;
+        public Calculation(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+    }
+}
---
+++ LineComparision/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LineComparison
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("Enter the value if x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value if x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value if y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value if y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Calculation lengthCal1 = new Calculation(x1, y1, x2, y2);
+            double line1 = lengthCal1.Length();
+            Calculation lengthCal2 = new Calculation(5, 6, 7, 8);
+            double line2 = lengthCal2.Length();
+            lengthCal2.EqualityCheck(line1, line2);
+        }
+
+    }
+}
