c7ec016f0837682ecb160c8c1c6a69c7747399ec
Mankar123sayu <125425650+Mankar123sayu@users.noreply.github.com>
2023-05-10 22:57:30 +0530

    Update Calculation.cs

--- LineComparision/Calculation.cs
+++ LineComparision/Calculation.cs
@@ -8,19 +8,7 @@ namespace LineComparision
 {
     public class Calculation
     {
-        int x1, x2, y1, y2;
-        public Calculation(int x1, int x2, int y1, int y2)
-        {
-            this.x1 = x1;
-            this.x2 = x2;
-            this.y1 = y1;
-            this.y2 = y2;
-        }
-        public void Length()
-        {
-            double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine(result);
-        }
+      
         public void EqualityCheck(double line1, double line2)
         {
             if (line1.Equals(line2))
